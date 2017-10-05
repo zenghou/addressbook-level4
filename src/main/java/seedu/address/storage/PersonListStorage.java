885db965d155011ee6a5aff1ddd2e3fdd05165f6
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-05 22:37:06 +0800

    PersonListStorage: Add skeleton methods based on AddressStorage

--- src/main/java/seedu/address/storage/PersonListStorage.java
+++ src/main/java/seedu/address/storage/PersonListStorage.java
@@ -1,4 +1,19 @@
 package seedu.address.storage;
 
+import java.util.List;
+
+import seedu.address.model.person.ReadOnlyPerson;
+
 public interface PersonListStorage {
+
+    public String getPersonListFilePath();
+
+    public List<ReadOnlyPerson> readPersonList();
+
+    public List<ReadOnlyPerson> readPersonList(String filePath);
+
+    public void savePersonList(List<ReadOnlyPerson> persons);
+
+    public void savePersonList(List<ReadOnlyPerson> persons, String filePath);
+
 }
--- src/main/java/seedu/address/storage/XmlPersonListStorage.java
+++ src/main/java/seedu/address/storage/XmlPersonListStorage.java
@@ -1,4 +1,34 @@
 package seedu.address.storage;
 
+import java.util.List;
+
+import seedu.address.model.person.ReadOnlyPerson;
+
 public class XmlPersonListStorage implements PersonListStorage{
+
+    private String filePath;
+
+    public XmlPersonListStorage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getPersonListFilePath() {
+        return this.filePath;
+    }
+
+    public List<ReadOnlyPerson> readPersonList() {
+        return readPersonList(this.filePath);
+    }
+
+    public List<ReadOnlyPerson> readPersonList(String filePath) {
+        return null;
+    }
+
+    public void savePersonList(List<ReadOnlyPerson> persons) {
+        savePersonList(persons, this.filePath);
+    }
+
+    public void savePersonList(List<ReadOnlyPerson> persons, String filePath) {
+
+    }
 }
