6a02f253a91b660f627b1ec1b66477c0371f8dce
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-10-06 09:43:36 +0800

    Add and implement command alias for FindCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -67,6 +67,7 @@ public class AddressBookParser {
             return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
+        case FindCommand.COMMAND_ALIAS:
             return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
