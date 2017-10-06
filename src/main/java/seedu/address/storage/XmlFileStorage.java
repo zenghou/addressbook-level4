6e05b73ef05484327a5915c63c2ab8fba7959fdb
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-06 11:00:20 +0800

    XmlFileStorage: Add load person list from file

--- src/main/java/seedu/address/storage/XmlFileStorage.java
+++ src/main/java/seedu/address/storage/XmlFileStorage.java
@@ -36,4 +36,16 @@ public class XmlFileStorage {
         }
     }
 
+    /**
+     * Returns person list in the file or an empty address book
+     */
+    public static XmlSerializablePersonList loadPersonListFromSaveFile(File file) throws DataConversionException,
+        FileNotFoundException {
+        try {
+            return XmlUtil.getDataFromFile(file, XmlSerializablePersonList.class);
+        } catch (JAXBException e) {
+            throw new DataConversionException(e);
+        }
+    }
+
 }
