44eaf918f017e63a84510fff4e3d4aa459dde6d9
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-10-06 09:44:32 +0800

    Add and implement command alias for DeleteCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -60,6 +60,7 @@ public class AddressBookParser {
             return new SelectCommandParser().parse(arguments);
 
         case DeleteCommand.COMMAND_WORD:
+        case DeleteCommand.COMMAND_ALIAS:
             return new DeleteCommandParser().parse(arguments);
 
         case ClearCommand.COMMAND_WORD:
