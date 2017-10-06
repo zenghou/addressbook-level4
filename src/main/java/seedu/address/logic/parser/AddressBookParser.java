345e1a81471db87b21b0b554e54ceedc014876e1
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-10-06 09:39:59 +0800

    Add and implement command alias for ListCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -70,6 +70,7 @@ public class AddressBookParser {
             return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
+        case ListCommand.COMMAND_ALIAS:
             return new ListCommand();
 
         case HistoryCommand.COMMAND_WORD:
