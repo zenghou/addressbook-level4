e95992e4a75a4ba05e3aa17ab41326dc08ff6e22
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-07 18:27:47 +0800

    XmlPersonListStorageTest: Add typical PersonList for testing

--- src/test/java/seedu/address/storage/XmlPersonListStorageTest.java
+++ src/test/java/seedu/address/storage/XmlPersonListStorageTest.java
@@ -4,24 +4,43 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.testutil.TypicalPersons;
 
 public class XmlPersonListStorageTest {
 
     private static final String TEST_DATA_FOLDER = FileUtil.getPath("./src/test/data/XmlPersonListStorageTest/");
+    private static final List<ReadOnlyPerson> typicalPersonList = new ArrayList<>();
+    private static final UniquePersonList typicalUniquePersonList = new UniquePersonList();
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Before
+    public void init() {
+        typicalPersonList.add(TypicalPersons.ALICE);
+        typicalPersonList.add(TypicalPersons.BENSON);
+        typicalPersonList.add(TypicalPersons.CARL);
+        typicalPersonList.add(TypicalPersons.DANIEL);
+        typicalPersonList.add(TypicalPersons.ELLE);
+        try {
+            typicalUniquePersonList.setPersons(typicalPersonList);
+        } catch (IllegalValueException e) {
+            assert false : "not possible";
+        }
+    }
 
     @Test
     public void savePersonList_nullPersonList_throwsNullPointerException() {
