caf9661f5128f1a478f96f00932bec77e782bebf
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-09 15:02:26 +0800

    XmlPersonListStorage: Change comments

--- src/main/java/seedu/address/storage/PersonListStorage.java
+++ src/main/java/seedu/address/storage/PersonListStorage.java
@@ -20,7 +20,7 @@ public interface PersonListStorage {
     public String getPersonListFilePath();
 
     /**
-     * Returns a list of {@link ReadOnlyPerson}.
+     * Returns person list data as {@link UniquePersonList}.
      *   Returns {@code Optional.empty()} if storage file is not found.
      * @throws DataConversionException if the data in storage is not in the expected format.
      * @throws IOException if there was any problem when reading from the storage.
