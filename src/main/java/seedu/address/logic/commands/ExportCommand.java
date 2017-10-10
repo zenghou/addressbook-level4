804d2413498eecb329f9a4efc92db46c519c284b
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-10 16:33:16 +0800

    Add new class ExportCommand

---
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Export a person list to a specified save file.
+ */
+public class ExportCommand extends UndoableCommand {
+
+    @Override
+    protected CommandResult executeUndoableCommand() throws CommandException {
+        return null;
+    }
+}
