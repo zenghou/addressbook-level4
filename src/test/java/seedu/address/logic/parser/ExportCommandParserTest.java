d509d83f4081ae1e52e646b379250eebb5946de3
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-12 01:38:13 +0800

    ExportCommandTest: Add success test

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -1,12 +1,24 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
 import org.junit.Test;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.FileUtil;
+import seedu.address.logic.commands.ExportCommand;
+
 public class ExportCommandParserTest {
+
+    private static final String TEST_DATA_FOLDER = FileUtil.getPath("./src/test/data/export/");
     private ExportCommandParser parser = new ExportCommandParser();
 
     @Test
     public void parser_validArgs_success() {
-
+        String filePath = TEST_DATA_FOLDER + "TypicalExportFile.xml";
+        Index[] indexes = {Index.fromOneBased(1), Index.fromOneBased(2)};
+        String input = "1, 2; " + filePath;
+        ExportCommand exportCommand = new ExportCommand(indexes, filePath);
+        assertParseSuccess(parser, input, exportCommand);
     }
 }
