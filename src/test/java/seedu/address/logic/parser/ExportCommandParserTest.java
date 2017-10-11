fbc3aa51d3fcad3aae391af7343086efb5a22f30
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-11 23:54:40 +0800

    Add test for ExportCommandParser

---
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+
+public class ExportCommandParserTest {
+    private ExportCommandParser parser = new ExportCommandParser();
+
+    @Test
+    public void parser_validArgs_success() {
+
+    }
+}
