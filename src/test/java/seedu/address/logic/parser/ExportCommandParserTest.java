374ae5316e0e8a6b839eb4d08417b30f9ea799c7
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-12 02:24:53 +0800

    ExportCommandParserTest: Extract method to get Index list

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -2,13 +2,13 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.FileUtil;
 import seedu.address.logic.commands.ExportCommand;
 
 public class ExportCommandParserTest {
@@ -18,12 +18,16 @@ public class ExportCommandParserTest {
     @Test
     public void parser_validArgs_success() {
         String filePath = "SomeFile.xml ";
-        List<Index> indexes = new ArrayList<>();
-        indexes.add(Index.fromOneBased(1));
-        indexes.add(Index.fromOneBased(2));
-        indexes.add(Index.fromOneBased(3));
+        List<Index> indexes = getIndexListFromOneBasedArray(new Integer[]{1, 2, 3});
         String input = "1, 2 3 ; " + filePath;
         ExportCommand exportCommand = new ExportCommand(indexes, filePath.trim());
         assertParseSuccess(parser, input, exportCommand);
+    }
+
+    /**
+     * Converts an One-based {@link Integer} Array to an {@link Index} List.
+     */
+    private List<Index> getIndexListFromOneBasedArray(Integer[] integers) {
+        return Arrays.stream(integers).map(Index::fromOneBased).collect(Collectors.toList());
     }
 }
