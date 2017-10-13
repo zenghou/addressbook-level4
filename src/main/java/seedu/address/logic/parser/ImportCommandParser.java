ea207ce3e58dfb035028eee2bd054ade18f78f27
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-13 17:49:07 +0800

    ImportCommand: Add dummy execution method
    ImportCommandParser: Add dummy parse method

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -1,7 +1,19 @@
 package seedu.address.logic.parser;
 
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 /**
  * Parses input argument to a file path and creates a new ImportCommand.
  */
 public class ImportCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of ImportCommand
+     * and returns an ImportCommand object for execution.
+     * @throws ParseException if user's input does not confirm the expected format
+     */
+    public ImportCommand parse(String args) throws ParseException {
+        return null;
+    }
 }
