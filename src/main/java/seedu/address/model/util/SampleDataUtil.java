3633b94999377704b55c9e6c166da979afc177b9
Unknown <zenghoulim@outlook.com>
2017-10-13 22:35:57 +0800

    Remove trailing whitespace

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,7 +24,7 @@ public class SampleDataUtil {
         try {
             personList[0] = new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Address("Blk 30 Geylang Street 29, #06-40"), getTagSet("friends"));
-            personList[1] = new Person(new Name("Bernice Yu"), new Phone("99272758"), 
+            personList[1] = new Person(new Name("Bernice Yu"), new Phone("99272758"),
                     new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                     getTagSet("colleagues", "friends"));
