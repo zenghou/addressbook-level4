3616a7d2150ea0edf9e6473f356e6d0334c27d5c
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-14 03:23:56 +0800

    ImportCommandTest: Add test for equals

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,6 +18,42 @@ public class ImportCommandTest {
 
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void equals() {
+        String filePath = "SomeFile.xml";
+        ImportCommand importCommand = new ImportCommand(filePath);
+
+        // the same object -> true
+        assertTrue(importCommand.equals(importCommand));
+
+        // different object same value -> true
+        assertTrue(importCommand.equals(new ImportCommand(filePath)));
+
+        // null -> false
+        assertFalse(importCommand.equals(null));
+
+        // different classes -> false
+        assertFalse(importCommand.equals(1));
+
+        // different values -> false
+        assertFalse(importCommand.equals(new ImportCommand("OtherFile.xml")));
+    }
+
+    @Test
+    public void execute_missingFile_throwsFileNotFoundException() throws Exception {
+
+    }
+
+    @Test
+    public void execute_notXmlFormat_throwsDataConversionException() throws Exception {
+
+    }
+
+    @Test
+    public void execute_emptyFile_failure() {
+
+    }
 
     @Test
     public void execute_validFilePathAndFile_success() {
