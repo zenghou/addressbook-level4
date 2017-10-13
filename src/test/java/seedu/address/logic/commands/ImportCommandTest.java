0a3883e849f32082439621e79a05b4fcef06b341
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-14 02:09:19 +0800

    Add test class for ImportCommand

---
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+
+import seedu.address.commons.util.FileUtil;
+
+public class ImportCommandTest {
+    private static final String TEST_DATA_FOLDER = FileUtil.getPath("./src/test/data/XmlPersonListStorageTest/");
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void execute_validFilePathAndFile_success() {
+
+    }
+}
