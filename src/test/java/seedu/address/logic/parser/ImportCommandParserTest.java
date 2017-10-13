26902841ff6fe3c7cc4dbafafc95afb764033a52
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-13 18:18:34 +0800

    Creat test class for ImportCommandParser

---
+++ src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+
+public class ImportCommandParserTest {
+
+    private ImportCommandParser parser = new ImportCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParserException() {
+
+    }
+
+    @Test
+    public void parse_validArg_success() {
+
+    }
+}
