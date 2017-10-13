73cb65727e85710d30efb97effb5171b62c77015
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-13 18:23:46 +0800

    ImportCommandParserTest: Add test for empty arg

--- src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
@@ -1,14 +1,26 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 public class ImportCommandParserTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private ImportCommandParser parser = new ImportCommandParser();
 
     @Test
-    public void parse_emptyArg_throwsParserException() {
-
+    public void parse_emptyArg_throwsParserException() throws Exception {
+        thrown.expect(ParseException.class);
+        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE));
+        parser.parse("  ");
     }
 
     @Test
