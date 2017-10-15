4649286a0c53d366c33fc034a476a586466e82af
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-15 23:18:39 +0800

    Add system test for ImportCommand

---
+++ src/test/java/systemtests/ImportCommandSystemTest.java
@@ -0,0 +1,16 @@
+package systemtests;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ImportCommandSystemTest extends AddressBookSystemTest {
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void importCommand() {
+
+    }
+}
