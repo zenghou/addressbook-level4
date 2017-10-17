a81dac235041f593b4c034e071ad428d9b0807ae
Unknown <zenghoulim@outlook.com>
2017-10-17 15:30:43 +0800

    Update SearchCommandTest

--- src/test/java/seedu/address/logic/commands/SearchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SearchCommandTest.java
@@ -1,24 +1,34 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.FIONA;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.person.DetailsContainKeyphrasePredicate;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.ReadOnlyPerson;
 
 public class SearchCommandTest {
-
-    ExpectedException expectedException = ExpectedException.none();
-
-    /** Test should throw Exception since execute method is not implemented */
-    @Test
-    public void execute_SearchCommand_throwsException() throws Exception {
-        expectedException.expect(Exception.class);
-        SearchCommand searchCommand = new SearchCommand(new DetailsContainKeyphrasePredicate(""));
-        searchCommand.execute();
-    }
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void equals() {
@@ -34,5 +44,41 @@ public class SearchCommandTest {
 
         // different type -> returns false
         assertFalse(searchCommandOne.equals(1));
+
+        // null -> returns false
+        assertFalse(searchCommandOne.equals(null));
+    }
+
+    @Test
+    public void execute_keyPhrase_singlePersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        SearchCommand command = prepareCommand("Alice Pauline");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(ALICE));
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code SearchCommand}.
+     */
+    private SearchCommand prepareCommand(String userInput) {
+        String preppedInput = userInput.trim();
+        SearchCommand command =
+                new SearchCommand(new DetailsContainKeyphrasePredicate(preppedInput));
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+
+    /**
+     * Asserts that {@code command} is successfully executed, and<br>
+     *     - the command feedback is equal to {@code expectedMessage}<br>
+     *     - the {@code FilteredList<ReadOnlyPerson>} is equal to {@code expectedList}<br>
+     *     - the {@code AddressBook} in model remains the same after executing the {@code command}
+     */
+    private void assertCommandSuccess(SearchCommand command, String expectedMessage, List<ReadOnlyPerson> expectedList) {
+        AddressBook expectedAddressBook = new AddressBook(model.getAddressBook());
+        CommandResult commandResult = command.execute();
+
+        assertEquals(expectedMessage, commandResult.feedbackToUser);
+        assertEquals(expectedList, model.getFilteredPersonList());
+        assertEquals(expectedAddressBook, model.getAddressBook());
     }
 }
