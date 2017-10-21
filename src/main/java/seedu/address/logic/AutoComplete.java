bc31b581fd49b0c125cc2acf05d80554002cc297
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-21 13:51:08 +0800

    Add auto complete api

--- src/main/java/seedu/address/logic/AutoComplete.java
+++ src/main/java/seedu/address/logic/AutoComplete.java
@@ -5,4 +5,48 @@ package seedu.address.logic;
  */
 public class AutoComplete {
 
+    public void autoComplete(String command) {
+        String[] args = command.split("\\s+");
+    }
+
+    public void addCommandAutoComplete(String args) {
+
+    }
+
+    public String deleteCommandAutoComplete(String args) {
+        return " ";
+    }
+
+    public String editCommandAutoComplete(String args) {
+
+    }
+
+    public String exportCommandAutoComplete(String args) {
+
+    }
+
+    public String findCommandAutoComplete(String args) {
+
+    }
+
+    public String importCommandAutoComplete(String args) {
+
+    }
+
+    public String remarkCommandAutoComplete(String args) {
+
+    }
+
+    public String searchCommandAutoComplete(String args) {
+
+    }
+
+    public String selectCommandAutoComplete(String args) {
+
+    }
+
+    public String sortCommandAutoComplete(String args) {
+
+    }
+
 }
