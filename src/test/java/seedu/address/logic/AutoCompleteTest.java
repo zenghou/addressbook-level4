1d045b831ac1c15a3482e7c48834f3a7117a5e37
HanYaodong <hanyaodongcn@hotmail.com>
2017-10-22 21:06:45 +0800

    Add test cases for add auto-complete

--- src/test/java/seedu/address/logic/AutoCompleteTest.java
+++ src/test/java/seedu/address/logic/AutoCompleteTest.java
@@ -1,8 +1,11 @@
 package seedu.address.logic;
 
+import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -10,6 +13,36 @@ import seedu.address.model.UserPrefs;
 
 public class AutoCompleteTest {
 
+    @Rule
+    private static final ExpectedException thrown = ExpectedException.none();
+
     private static final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
+    @Test
+    public void autoCompleteAdd_missingOneField_addMissingPrefix() {
+
+    }
+
+    @Test
+    public void autoCompleteAdd_missingMultipleFields_addMissingPrefixes() {
+
+    }
+
+    @Test
+    public void autoCompleteAdd_invalidFields_addMissingPrefixes() {
+
+    }
+
+    @Test
+    public void autoCompleteAdd_allValidFields_noChange() {
+
+    }
+
+    /**
+     * Asserts if the auto-complete of {@code command} equals to {@code expectedResult}.
+     */
+    private void assertAutoComplete(String command, String expectedResult) {
+        String result = AutoComplete.autoComplete(command, model);
+        assertEquals(result, expectedResult);
+    }
 }
