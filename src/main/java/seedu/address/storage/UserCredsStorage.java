195ca9b925a1e1990a19617c612202e86399995d
Unknown <zenghoulim@outlook.com>
2017-10-25 00:46:03 +0800

    Add JsonUserCredsStorage
    - Interface as API

---
+++ src/main/java/seedu/address/storage/UserCredsStorage.java
@@ -0,0 +1,34 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.user.UserCreds;
+
+/**
+ * Represents a storage for {@link seedu.address.model.user.UserCreds}.
+ */
+public interface UserCredsStorage {
+
+    /**
+     * Returns the file path of the UserCreds data file.
+     */
+    String getUserCredsFilePath();
+
+    /**
+     * Returns UserCreds data from storage.
+     *   Returns {@code Optional.empty()} if storage file is not found.
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException if there was any problem when reading from the storage.
+     */
+    Optional<UserCreds> readUserCreds() throws DataConversionException, IOException;
+
+    /**
+     * Saves the given {@link UserCreds} to the storage.
+     * @param userCreds cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveUserCreds(UserCreds userCreds) throws IOException;
+
+}
