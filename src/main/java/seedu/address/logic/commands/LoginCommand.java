cfa5c6f416de6818160dbc1b20c0c3080664cd5e
Unknown <zenghoulim@outlook.com>
2017-10-24 21:17:16 +0800

    LoginCommand: add constructor and message usage
    - Temporarily use String to store username and password 
    - Should implement Credentials class to hide data

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -1,10 +1,27 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+
 public class LoginCommand extends Command{
     public static final String COMMAND_WORD = "login";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD  + ": Authenticates a user of the address book."
+            + "Parameters: "
+            + PREFIX_USERNAME + "USERNAME "
+            + PREFIX_PASSWORD + "PASSWORD ";
+
     public static final String MESSAGE_SUCCESS = "Successfully logged in!";
-    
+
+    private final String username;
+    private final String password;
+
+    //TODO: Implement Credentials class so that username and password not stored in open
+    public LoginCommand(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     @Override
     public CommandResult execute() {
         // TODO: replace stub method
