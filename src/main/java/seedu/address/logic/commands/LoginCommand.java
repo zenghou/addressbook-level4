0f124c80be77052e8983e7ff3c92ff5ab4906df9
Unknown <zenghoulim@outlook.com>
2017-10-25 04:08:38 +0800

    LoginCommand: Replace String variables with UserCred

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
 
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.user.UserCreds;
+
 /**
  * Authenticates a user with username and password
  */
@@ -16,24 +19,27 @@ public class LoginCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Successfully logged in!";
 
-    private final String username;
-    private final String password;
+    UserCreds userCreds;
 
     //TODO: Implement Credentials class so that username and password not stored in open
     public LoginCommand(String username, String password) {
-        this.username = username;
-        this.password = password;
+        this.userCreds = new UserCreds(username, password);
+
     }
 
-    public CommandResult execute() {
-        // TODO: replace stub method
-        return new CommandResult("stub");
+    public CommandResult execute() throws CommandException {
+        // TODO: check if user and password is equals to UserCreds
+        boolean isVerifiedUser = userCreds.equals(model.getUserCreds());
+        if (isVerifiedUser) {
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
+        throw new CommandException("Please ensure that username and password are entered correctly!");
     }
 
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof LoginCommand // instanceof handles nulls
-                && username.equals(((LoginCommand) other).username)
-                && password.equals(((LoginCommand) other).password));
+                && userCreds.equals(((LoginCommand) other).userCreds));
     }
+
 }
