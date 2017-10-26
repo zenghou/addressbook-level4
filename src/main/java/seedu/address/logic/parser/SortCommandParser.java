d619c00b34e8ca42e02ca0b6240dfd5efdfdfd92
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-10-26 15:16:58 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -2,9 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME_FOR_SORTING;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE_FOR_SORTING;
 
 import java.util.function.Consumer;
