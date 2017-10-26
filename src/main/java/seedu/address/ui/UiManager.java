84950add7c7625b1a4051538730ae8be4b609817
Unknown <zenghoulim@outlook.com>
2017-10-26 23:06:55 +0800

    UiManager: remove fillInnerParts
    - temporarily remove UI changes to application

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -59,7 +59,7 @@ public class UiManager extends ComponentManager implements Ui {
         try {
             mainWindow = new MainWindow(primaryStage, config, prefs, logic);
             mainWindow.show(); //This should be called before creating other UI parts
-            mainWindow.fillCommandBoxAndDisplayPanel();
+            mainWindow.fillInnerParts();
 
         } catch (Throwable e) {
             logger.severe(StringUtil.getDetails(e));
