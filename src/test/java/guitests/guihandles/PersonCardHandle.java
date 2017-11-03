50d6c56ba11269e4cd3f81bda802f2ec77fd494b
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-11-03 09:02:02 +0800

    Remove unused import

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -1,11 +1,7 @@
 package guitests.guihandles;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
 
 /**
  * Provides a handle to a person card in the person list panel.
