e31142a5d5f7794d63c65e974f57304c4f86b350
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-11-10 04:42:12 +0800

    Update to follow checkstyle

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.Test;
 
@@ -15,7 +14,7 @@ import seedu.address.logic.commands.SortCommand;
 public class SortCommandParserTest {
     
     private SortCommandParser parser = new SortCommandParser();
-
+    
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, " ",
